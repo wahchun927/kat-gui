69f7b8b57326533bf4812f1ad347ef579d703894
Chris Boesch <scboesch@gmail.com>
2013-05-24 10:35:48 +0800

    Travis-ci comment to watch for failing builds

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -28,8 +28,8 @@ describe('ViTech provided tests for index loaded partials', function() {
       
       expect(browser().location().url()).toBe("/quests");
       //You can select all the text from all h5 or any other html element
-      //expect(element('.ng-binding').text()).
-      //  toMatch("Welcome, Ruijun!");
+      expect(element('#myCarousel .ng-scope:nth-child(1) .ng-binding').text()).
+        toMatch("Incorrect Title");//"The Spy Who Coded"
       if (pauseAll) pause();
   });
 
