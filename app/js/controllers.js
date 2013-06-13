25005d6e7eeaae38ca203c08607f3eb96961eb83
Arathi <arathi.2010@sis.smu.edu.sg>
2013-06-14 05:21:26 +0800

    ranking 7"

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -195,6 +195,14 @@ function PathController($scope,$resource,$cookieStore,$location){
       $(this).addClass('selected');
       
     });
+	
+	$scope.pathSelectRankSmall=function(){
+    $('#myCarouselRankSmall input:image').click(function() {
+      $('#myCarouselRankSmall input:image').removeClass('selected');   
+      $(this).addClass('selected');
+      
+    });
+  }
   }
 	//assign the level number to the buttons
 	$scope.setButton=function(name,problemID){
