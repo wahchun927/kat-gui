462204ec1cf27e767834409c5096f7c8af532502
Chris from Travis <cboesch@smu.edu.sg>
2013-08-02 09:46:39 +0000

    Updating from Travis-ci

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,25 @@
 'use strict';
 var myApp = angular.module('myApp', ['myAppConfig']);
+
+myApp.filter('groupBy', function() {
+    return function(items, groupedBy) {
+        if (items) {
+            var finalItems = [],
+                thisGroup;
+            for (var i = 0; i < items.length; i++) {
+                if (!thisGroup) {
+                    thisGroup = [];
+                }
+                thisGroup.push(items[i]);
+                if (((i+1) % groupedBy) == 0) {
+                    finalItems.push(thisGroup);
+                    thisGroup = null;
+                }
+            };
+            if (thisGroup) {
+                finalItems.push(thisGroup);
+            }
+            return finalItems;
+        }
+    };
+});
