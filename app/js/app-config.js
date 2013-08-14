555103abf549f115ca9bea3dd20a36aa43165451
Chris from Travis <cboesch@smu.edu.sg>
2013-08-14 11:40:49 +0000

    Updating from Travis-ci

--- app/js/app-config.js
+++ app/js/app-config.js
@@ -33,3 +33,26 @@ myAppConfig.filter('startFrom', function() {
         }
     };
 });
+
+myAppConfig.filter('groupBy', function() {
+    return function(items, groupedBy) {
+        if (items) {
+            var finalItems = [],
+                thisGroup;
+            for (var i = 0; i < items.length; i++) {
+                if (!thisGroup) {
+                    thisGroup = [];
+                }
+                thisGroup.push(items[i]);
+                if (((i+1) % groupedBy) == 0) {
+                    finalItems.push(thisGroup);
+                    thisGroup = null;
+                }
+            };
+            if (thisGroup) {
+                finalItems.push(thisGroup);
+            }
+            return finalItems;
+        }
+    };
+});
