9a04b8c0835c93fd743520ed38abc1bcb7fb96dd
Chris from Travis <cboesch@smu.edu.sg>
2013-09-30 16:01:10 +0000

    Updating from Travis-ci

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1375,6 +1375,22 @@ function ChallengeController($scope,$resource,$location,$cookieStore,$http,$rout
 
 		var todayDate= dd+'/'+ mm +'/'+yyyy;
 		
+			//derive date difference from start date and end date
+			var date1 = new Date(sDate.split("/").reverse().join("/"));
+			var date2 = new Date(eDate.split("/").reverse().join("/"));
+			
+			//// The number of milliseconds in one day
+			var ONE_DAY = 1000 * 60 * 60 * 24
+
+			// Convert both dates to milliseconds
+			var date1_ms = date1.getTime()
+			var date2_ms = date2.getTime()
+
+			// Calculate the difference in milliseconds
+			var difference_ms = Math.abs(date1_ms - date2_ms)
+			
+			var totalNumberOfDays=Math.round(difference_ms/ONE_DAY)+1;
+			
 		if($scope.challengeToEdit.challenge.allowedCountries.length=="0" ){
 			$scope.challengeToEdit.challenge.worldwide = 1;
 			$scope.challengeToEdit.challenge.allowedCountries = [];
@@ -1458,7 +1474,7 @@ function ChallengeController($scope,$resource,$location,$cookieStore,$http,$rout
 				else{
 					$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 					$http.post('/jsonapi/save_edit_challenge/'+challenge_id, {
-									totalDays:$scope.challengeToEdit.challenge.totalDays,
+									totalDays:totalNumberOfDays,
 									problemsPerDay:$scope.challengeToEdit.challenge.problemsPerDay,
 									difficulty:$scope.challengeToEdit.challenge.difficulty,
 									pathID:$scope.chaPathID,
